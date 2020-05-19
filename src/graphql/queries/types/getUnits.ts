2199869a912b9c1630cbdc6714be00b4edac8306
ayankovsky <p44gr10@gmail.com>
2020-05-20 00:48:51 +0200

    Added Units Container

---
+++ src/graphql/queries/types/getUnits.ts
@@ -0,0 +1,34 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: getUnits
+// ====================================================
+
+export interface getUnits_getUnits_units {
+  __typename: "UnitListDetails";
+  unitName: string;
+  invocations: string;
+  errors: string;
+  errorRate: number | null;
+  averageDuration: number | null;
+}
+
+export interface getUnits_getUnits {
+  __typename: "GetUnitsResponse";
+  units: getUnits_getUnits_units[];
+  offset: number;
+  hasMore: boolean;
+}
+
+export interface getUnits {
+  getUnits: getUnits_getUnits;
+}
+
+export interface getUnitsVariables {
+  search?: string | null;
+  offset?: number | null;
+  since: string;
+}
