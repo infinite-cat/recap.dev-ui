64b72778f119236045f00694b755c14aaf52096e
Eugene Draitsev <ddrrai@gmail.com>
2020-06-03 15:44:59 +0200

    Fixed date format

--- src/utils/date.utils.ts
+++ src/utils/date.utils.ts
@@ -4,4 +4,4 @@ export const formatDateTime = (time?: number | string) =>
   DateTime.fromMillis(Number(time)).toLocaleString(DateTime.DATETIME_SHORT)
 
 export const formatDateTimeShort = (time?: number | string) =>
-  DateTime.fromMillis(Number(time)).toFormat('dd LLL HH:MM')
+  DateTime.fromMillis(Number(time)).toFormat('dd LLL HH:mm')
