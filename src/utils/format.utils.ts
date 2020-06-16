69b41a12eadd9f08e47d80d68b9b97152ad1b684
Eugene Draitsev <ddrrai@gmail.com>
2020-06-16 22:08:17 +0200

    Fixed broken JSON viewer for array-like objects (#12)

--- src/utils/format.utils.ts
+++ src/utils/format.utils.ts
@@ -1,7 +1,18 @@
+import { isArray } from 'lodash-es'
+
 export const safeParse = (parseString: string) => {
   try {
     return JSON.parse(parseString)
   } catch (e) {
     return null
   }
+}
+
+export const formatJSONViewerInput = (src = '') => {
+  const parsed = safeParse(src)
+  if (!isArray(src)) {
+    const { length, ...formatted } = parsed
+    return JSON.stringify(formatted)
+  }
+  return JSON.stringify(src)
 }
