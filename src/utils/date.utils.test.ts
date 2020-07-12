d1936a03844f25f1852b08208fb4ab9517bbb67f
ayankovsky <p44gr10@gmail.com>
2020-07-12 17:50:04 +0200

    Added format duration

---
+++ src/utils/date.utils.test.ts
@@ -0,0 +1,15 @@
+import { formatDuration } from './date.utils'
+
+describe('date.utils.ts', () => {
+  describe('formatDuration', () => {
+    it.each([
+      [8, '8ms'],
+      [256, '256ms'],
+      [960000, '16m'],
+      [961111, '16m 1s 111ms'],
+      [11961111, '3h 19m 21s 111ms'],
+    ])('should format "%i" milliseconds to "%s"', (milliseconds, expected) => {
+      expect(formatDuration(milliseconds)).toBe(expected)
+    })
+  })
+})
--- src/utils/date.utils.ts
+++ src/utils/date.utils.ts
@@ -1,7 +1,38 @@
-import { DateTime } from 'luxon'
+import { DateTime, Duration, DurationUnit } from 'luxon'
+import { map, compact } from 'lodash-es'
 
 export const formatDateTime = (time?: number | string) =>
   DateTime.fromMillis(Number(time)).toLocaleString(DateTime.DATETIME_SHORT)
 
 export const formatDateTimeShort = (time?: number | string) =>
   DateTime.fromMillis(Number(time)).toFormat('dd LLL HH:mm')
+
+const units: DurationUnit[] = ['hours', 'minutes', 'seconds', 'milliseconds']
+const shortNames = ['h', 'm', 's', 'ms']
+
+export const formatDuration = (milliseconds: number) => {
+  const duration = Duration.fromObject({ milliseconds })
+    .shiftTo(...units)
+    .toObject()
+
+  const string = compact(
+    map(units, (unit, i) => (duration[unit] ? `${duration[unit]}${shortNames[i]}` : null)),
+  ).join(' ')
+
+  return string
+}
+
+const units: DurationUnit[] = ['hours', 'minutes', 'seconds', 'milliseconds']
+const shortNames = ['h', 'm', 's', 'ms']
+
+export const formatDuration = (milliseconds: number) => {
+  const duration = Duration.fromObject({ milliseconds })
+    .shiftTo(...units)
+    .toObject()
+
+  const string = compact(
+    map(units, (unit, i) => (duration[unit] ? `${duration[unit]}${shortNames[i]}` : null)),
+  ).join(' ')
+
+  return string
+}
