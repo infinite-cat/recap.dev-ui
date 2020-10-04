fdb1de5f98dd71f2dce59ac2eba0a1d06a111de3
Eugene Draitsev <ddrrai@gmail.com>
2020-10-04 21:20:19 +0200

    Fixed storybook build issue

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = ({ config }) => {
   config.module.rules.push({
@@ -10,7 +10,7 @@ module.exports = ({ config }) => {
   })
   // fonts
   config.module.rules.push({
-    test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+    test: /\.(png|woff|woff2|eot|ttf)$/,
     use: [
       {
         loader: 'file-loader',
@@ -22,5 +22,19 @@ module.exports = ({ config }) => {
     include: path.resolve(__dirname, '../'),
   })
   config.resolve.extensions.push('.ts', '.tsx')
+
+  const rules = config.module.rules
+  const fileLoaderRule = rules.find((rule) => rule.test.test('.js'))
+  fileLoaderRule.use[0].options.plugins.push([
+    require.resolve('babel-plugin-named-asset-import'),
+    {
+      loaderMap: {
+        svg: {
+          ReactComponent: '@svgr/webpack?-svgo,+titleProp,+ref![path]',
+        },
+      },
+    },
+  ])
+
   return config
 }
