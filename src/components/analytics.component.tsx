9259dd4891b087aebc519b0651865224c76891cd
Eugene Draitsev <ddrrai@gmail.com>
2021-02-16 19:27:24 +0100

    Added analytics script

--- src/components/analytics.component.tsx
+++ src/components/analytics.component.tsx
@@ -1,12 +1,19 @@
 import React, { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
+import { Helmet } from 'react-helmet'
+
+import { BASE_URL } from '../constants'
 
 export const Analytics = () => {
   const location = useLocation()
 
   useEffect(() => {
-    window?.ga?.send(['pageview', location.pathname])
+    window?.ga?.('send', 'pageview', location.pathname)
   }, [location])
 
-  return <></>
+  return (
+    <Helmet>
+      <script src={`${BASE_URL}/analytics.js`} />
+    </Helmet>
+  )
 }
