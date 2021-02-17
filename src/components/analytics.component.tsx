0643e12d3108784e95a85190fa04504c575cac0d
Eugene Draitsev <ddrrai@gmail.com>
2021-02-17 22:18:47 +0100

    Removed ga cookies (#31)

--- src/components/analytics.component.tsx
+++ src/components/analytics.component.tsx
@@ -3,9 +3,15 @@ import { useLocation } from 'react-router-dom'
 import { Helmet } from 'react-helmet'
 
 import { BASE_URL } from '../constants'
+import { deleteAllCookies, listenCookieChange } from '../utils'
 
 export const Analytics = () => {
   const location = useLocation()
+
+  useEffect(() => {
+    const interval = listenCookieChange(deleteAllCookies)
+    return () => clearInterval(interval)
+  }, [])
 
   useEffect(() => {
     window?.gtag?.('event', 'page_view', {
